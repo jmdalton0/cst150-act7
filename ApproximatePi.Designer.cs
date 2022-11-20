1e85f342f202bb4edc65826954012281d7149c18
Jesse Dalton <jmdalton0@gmail.com>
2022-11-20 13:50:54 -0800

    initial commit

---
+++ ApproximatePi.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Act7
+{
+    partial class ApproximatePi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ApproximatePi));
+            this.enterLabel = new System.Windows.Forms.Label();
+            this.numTermsTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.resultValueLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterLabel
+            // 
+            this.enterLabel.AutoSize = true;
+            this.enterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterLabel.Location = new System.Drawing.Point(59, 48);
+            this.enterLabel.Name = "enterLabel";
+            this.enterLabel.Size = new System.Drawing.Size(135, 20);
+            this.enterLabel.TabIndex = 0;
+            this.enterLabel.Text = "Enter # of terms:";
+            // 
+            // numTermsTextBox
+            // 
+            this.numTermsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numTermsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numTermsTextBox.Location = new System.Drawing.Point(216, 46);
+            this.numTermsTextBox.Name = "numTermsTextBox";
+            this.numTermsTextBox.Size = new System.Drawing.Size(254, 27);
+            this.numTermsTextBox.TabIndex = 1;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(59, 219);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(289, 20);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "Approximate value of pi after 0 terms:";
+            // 
+            // calcButton
+            // 
+            this.calcButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.ForeColor = System.Drawing.Color.White;
+            this.calcButton.Location = new System.Drawing.Point(63, 119);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(131, 58);
+            this.calcButton.TabIndex = 3;
+            this.calcButton.Text = "CALCULATE";
+            this.calcButton.UseVisualStyleBackColor = false;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // resultValueLabel
+            // 
+            this.resultValueLabel.AutoSize = true;
+            this.resultValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultValueLabel.Location = new System.Drawing.Point(59, 269);
+            this.resultValueLabel.Name = "resultValueLabel";
+            this.resultValueLabel.Size = new System.Drawing.Size(225, 20);
+            this.resultValueLabel.TabIndex = 4;
+            this.resultValueLabel.Text = "Enter # of terms to calculate.";
+            // 
+            // ApproximatePi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(547, 326);
+            this.Controls.Add(this.resultValueLabel);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.numTermsTextBox);
+            this.Controls.Add(this.enterLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ApproximatePi";
+            this.Text = "Approximate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterLabel;
+        private System.Windows.Forms.TextBox numTermsTextBox;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label resultValueLabel;
+    }
+}
---
+++ ApproximatePi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Act7
+{
+    // form to approximate the value of pi
+    public partial class ApproximatePi : Form
+    {
+        public ApproximatePi()
+        {
+            InitializeComponent();
+        }
+
+        // update the number of terms displayed by the result label
+        private void updateResultLabel(int numTerms)
+        {
+            resultLabel.Text = String.Format("Approximate value of pi after {0} terms:", numTerms);
+        }
+
+        // update the result
+        private void updateResultValueLabel(double result)
+        {
+            resultValueLabel.Text = result.ToString();
+        }
+
+        // clean input and perform calculation on click
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            int numTerms = 0;
+            // if user input is a positive integer, perform calculation
+            if (int.TryParse(numTermsTextBox.Text, out numTerms) && numTerms > -1)
+            {
+                updateResultLabel(numTerms);
+
+                double result = 4.0;
+                // for each term
+                for (int i = 1; i < numTerms; i++) // start at 1 to make sure the result is 4 if the user enters 0 or 1
+                {
+                    // formula for each term
+                    double term = 4.0 / ((i * 2) + 1);
+                    // add if n is even
+                    if (i % 2 == 0)
+                    {
+                        result += term;
+                    }
+                    // subtract is n is odd
+                    else
+                    {
+                        result -= term;
+                    }
+                }
+
+                updateResultValueLabel(result);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive integer");
+            }
+        }
+    }
+}
